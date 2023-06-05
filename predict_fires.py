b3e72ff5cce69eeefd36e553ca51518547dd4c18
kepasu <kps@0x001.fritz.box>
2023-06-05 22:58:20 +0200

    vernünftige datenformatierung

--- predict_fires.py
+++ predict_fires.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from datetime import datetime, timedelta
+from concurrent.futures import ThreadPoolExecutor
+from searchdata import checkForFire, load_fire_data
 
 
 def iterate_days(start_year, end_year):
@@ -11,12 +13,50 @@ def iterate_days(start_year, end_year):
         print(current_date)
         current_date += timedelta(days=1)
 
+def check_fire(row, lat, lon):
+    return 1 if checkForFire(lat, lon, row.Date) else 0
+
+
+def add_fire_column(city_df, lat, lon):
+    # Add a new column 'fire' initialized with 0
+    city_df['fire'] = 0
+
+    # Load fire data
+
+    # Apply the checkForFire() function to each row in the DataFrame
+    city_df['fire'] = city_df.apply(lambda row: check_fire(row, lat, lon), axis=1)
+
+    return city_df
+
+
+def add_fire_column_old(city_df, lat, lon):
+    # Add a new column 'fire' initialized with 0
+    city_df['fire'] = 0
+
+    with ThreadPoolExecutor() as executor:
+        # Apply the checkForFire() function to each row in the DataFrame
+        results = list(executor.map(lambda row: check_fire(row, lat, lon, fire_data), city_df.itertuples()))
+
+    city_df['fire'] = results
+
+    return city_df
+
+def filter_dataframe_by_years(df, start_year, end_year):
+    # Convert the date column to datetime type
+    df['Date'] = pd.to_datetime(df['Date'])
+
+    # Filter the DataFrame based on the specified years
+    df = df.query(f"{start_year} <= Date.dt.year <= {end_year}").copy()
+
+    return df
+
+
 
 def return_df(data_path):
     df = pd.read_csv(data_path)
     return df
 
-
+# @profile
 def city_dataframes(weather_folder, start_year, end_year):
 
     info_df = pd.read_csv(weather_folder + '/city_info.csv')
@@ -30,8 +70,15 @@ def city_dataframes(weather_folder, start_year, end_year):
 
         city_id = row['ID']
         city_df = pd.read_csv(weather_folder + '/' + city_id + '.csv')
+        # keep only the data that is between the two given years
+        city_df = filter_dataframe_by_years(city_df, start_year, end_year)
         city_lat = row['Lat']
         city_lon = row['Lon']
+
+        # adds a column to the city_df that states
+        # if there has been a fire on that day
+        city_df = add_fire_column(city_df, city_lat, city_lon)
+
         city_dfs.setdefault(city_id, [])
         city_dfs[city_id].append(city_df)
 
@@ -44,7 +91,4 @@ def city_dataframes(weather_folder, start_year, end_year):
     return city_dfs
 
 
-weather_df = city_dataframes('data/weather', 2000, 2000)
-
-for key in weather_df:
-    print(key)
+print(city_dataframes('data/weather', 2021, 2021))
